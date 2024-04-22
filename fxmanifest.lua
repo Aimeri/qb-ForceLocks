338effd471e54bfb0035c3bbc968bc42512fb1b0
Aimeri <edntremblay@gmail.com>
2024-04-22 02:22:14 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
 fx_version 'clerulean'
 game 'gta5'
 
-author 'GNRLTailya#0842' --Discord Profile
+author '@aimeri' --Discord Profile
 description 'A script that forces vehicles to unlock and give keys to job related tasks. ex: Police, Mechanic'
 
-version '1.0.0'
+version '1.0.1'
 
 client_script 'client.lua'
 server_script 'server.lua'
