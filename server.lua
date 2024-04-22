45cac863b34e461ed2977b45c3a43ca379d7496a
Aimeri <edntremblay@gmail.com>
2024-04-22 02:20:41 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,6 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
+-- Set the blowtorch item as a usable item.
 QBCore.Functions.CreateUseableItem("blowtorch", function(source, item)
         TriggerClientEvent('forcelocks:client:forcelock', source)
 end)
